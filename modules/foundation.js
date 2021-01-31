50bc66480195d3bc9a108bae113bc3aa35230f23
Salva <Salva@ZEUS-HOME.local>
2021-01-31 06:35:54 -0500

    Initial commit

---
+++ modules/foundation.js
@@ -0,0 +1,43 @@
+// Processes operations related to the Sia Foundation subsidies and wallets
+var exports = module.exports={}
+var blake = require('blakejs')
+
+
+
+
+function calculateOutputIdSubsidy(blockHash, specifier) {
+	// This function takes the hash of a block together with the specifier of the Sia Foundation and calculates
+	// with it the OutputID of a Foundation subsidy. It reproduces what the Sia code does in Golang
+	// This function is a courtesy of Nate Maninger, from https://SiaCentral.com, whom I am extremely grateful with for his help
+	
+	var specifierFoundation = newSpecifier(specifier);
+
+	function newSpecifier(str) {
+		// Takes the specifier and transforms it into a buffer
+		const buf = Buffer.alloc(16);
+		buf.write(str, encoding='ascii');
+		return buf;
+	}
+
+	function hashAll(...args) {
+		// Calculates the subsidy ID by hashing together the block ID bytes and foundation specifier
+
+		const hasher = blake.blake2bInit(32, null);
+
+		// loop through each of our arguments and update the hasher
+		for (let i = 0; i < args.length; i++)
+		blake.blake2bUpdate(hasher, args[i]);
+
+		//finalize the hash
+		return blake.blake2bFinal(hasher);
+	}
+
+	// convert the block ID from its hex encoded string to a Buffer
+	var blockID = Buffer.from(blockHash, 'hex');
+	// calculate the subsidy id
+	var subsidyOutputID = hashAll(blockID, specifierFoundation);
+	// convert the Buffer to a hex encoded ID string
+	var  outputIDString = Buffer.from(subsidyOutputID).toString('hex');
+
+	return outputIDString
+}
--- modules/params.js
+++ modules/params.js
@@ -53,7 +53,14 @@ exports.Params = function(config, scriptPath)
         siafundFees: 0.039,
         totalSiafunds: 10000,
         dustThreshold: 1000000000000000000000, // Arbitrary. Bellow this threshold, we consider the amount as "dust" and wont compute it for balances of addresses. 1 milliSia by default
-        coinPrecision: 1000000000000000000000000 // How many Hastings make a coin
+        coinPrecision: 1000000000000000000000000, // How many Hastings make a coin
+        foundationForkHeight: 298000,
+        foundationInitialSubsidy: BigInt(1576800000000000000000000000000000),
+        foundationSubsidy: BigInt(129600000000000000000000000000000),
+        foundationSubsidyPeriodicity: 4320,
+        foundationInitialAddress = "053b2def3cbdd078c19d62ce2b4f0b1a3c5e0ffbeeff01280efb1f8969b2f5bb4fdc680f0807",
+        foundationInitialFailsafeAddress = "27c22a6c6e6645802a3b8fa0e5374657438ef12716d2205d3e866272de1b644dbabd53d6d560",
+        foundationSpecifier = "foundation" // String used to hash the OutputID of Foundation subsidies
     }
 
     // SQL database connection
